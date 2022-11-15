70807f0bd628a5aa47ca7aaa19ca083007aea0b2
Mike Valeriano <mkvlrn@gmail.com>
2022-11-15 09:37:54 -0300

    feat: get balance service

---
+++ src/backend/modules/accounts/services/get-balance.service.spec.ts
@@ -0,0 +1,61 @@
+import { createMock } from '@golevelup/ts-jest';
+import { PrismaClient } from '@prisma/client';
+
+import { GetBalanceService } from '#/backend/modules/accounts/services/get-balance.service';
+import { AppError } from '#/backend/server/AppError';
+
+describe('get-balance.service.ts', () => {
+  test('success', async () => {
+    const sut = new GetBalanceService(
+      createMock<PrismaClient>({
+        account: {
+          findUnique: jest.fn().mockResolvedValue({ id: '123', balance: 100 }),
+        },
+      }),
+    );
+
+    const result = await sut.execute('123');
+
+    expect(result).toEqual({ id: '123', balance: 100 });
+  });
+  test('fail - account not found', async () => {
+    const sut = new GetBalanceService(
+      createMock<PrismaClient>({
+        account: {
+          findUnique: jest.fn().mockResolvedValue(null),
+        },
+      }),
+    );
+
+    const act = () => sut.execute('123');
+
+    await expect(act).rejects.toMatchObject<AppError>({
+      name: 'AppError',
+      statusCode: 404,
+      type: 'NOT_FOUND',
+      message: 'account not found',
+      details: null,
+    });
+  });
+  test('fail - orm/database error', async () => {
+    const sut = new GetBalanceService(
+      createMock<PrismaClient>({
+        account: {
+          findUnique: jest.fn(() => {
+            throw new Error('database exploded');
+          }),
+        },
+      }),
+    );
+
+    const act = () => sut.execute('123');
+
+    await expect(act).rejects.toMatchObject<AppError>({
+      name: 'AppError',
+      statusCode: 500,
+      type: 'INTERNAL',
+      message: 'database exploded',
+      details: null,
+    });
+  });
+});
---
+++ src/backend/modules/accounts/services/get-balance.service.ts
@@ -0,0 +1,20 @@
+import { Account, PrismaClient } from '@prisma/client';
+
+import { AppError, AppErrorType } from '#/backend/server/AppError';
+
+export class GetBalanceService {
+  constructor(private orm: PrismaClient) {}
+
+  execute = async (id: string): Promise<Account> => {
+    try {
+      const result = await this.orm.account.findUnique({ where: { id } });
+      if (!result)
+        throw new AppError(AppErrorType.NOT_FOUND, 'account not found');
+
+      return result;
+    } catch (err) {
+      if (err instanceof AppError) throw err;
+      throw new AppError(AppErrorType.INTERNAL, (err as Error).message);
+    }
+  };
+}
