d36c5e92fd7eddb6b20422d6dcbf44ec771a00a8
Mike Valeriano <mkvlrn@gmail.com>
2022-11-16 09:21:27 -0300

    feat: add route to client

--- src/backend/server/AppRouter.ts
+++ src/backend/server/AppRouter.ts
@@ -1,6 +1,8 @@
-import { Router } from 'express';
+import express, { Response, Router } from 'express';
+import { join, resolve } from 'path';
 import { injectable } from 'tsyringe';
 
+import { CustomRequest } from '#/backend/interfaces/CustomRequest';
 import { AccountRouter } from '#/backend/modules/account/account.router';
 import { AuthRouter } from '#/backend/modules/auth/auth.router';
 import { UsersRouter } from '#/backend/modules/users/users.router';
@@ -14,8 +16,13 @@ export class AppRouter {
     private usersRouter: UsersRouter,
     private accountRouter: AccountRouter,
   ) {
-    this.routes.use('/auth', this.authRouter.routes);
-    this.routes.use('/users', this.usersRouter.routes);
-    this.routes.use('/account', this.accountRouter.routes);
+    this.routes.use('/api/auth', this.authRouter.routes);
+    this.routes.use('/api/users', this.usersRouter.routes);
+    this.routes.use('/api/account', this.accountRouter.routes);
+
+    this.routes.use(express.static(join(__dirname, '..', 'client')));
+    this.routes.get('/client', (_req: CustomRequest, res: Response) => {
+      res.sendFile(resolve(__dirname, '..', 'client', 'index.html'));
+    });
   }
 }
