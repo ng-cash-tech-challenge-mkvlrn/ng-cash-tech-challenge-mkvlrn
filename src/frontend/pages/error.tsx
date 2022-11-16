e4c3774a099453e3b0807b09eba728ec42901a49
Mike Valeriano <mkvlrn@gmail.com>
2022-11-16 14:09:18 -0300

    feat: replace fontawesome with tabler icons

--- src/frontend/pages/error.tsx
+++ src/frontend/pages/error.tsx
@@ -1,15 +1,18 @@
-import { Paper, Text, Title } from '@mantine/core';
+import { Group, Paper, Text, Title } from '@mantine/core';
+import { IconError404, IconForbid } from '@tabler/icons';
 import { Link } from 'react-router-dom';
 
 interface AppErrorProps {
   code: 401 | 404;
 }
 
+const iconSize = 36;
+
 const errorMessages = {
   401: {
     heading: 'unauthorized',
     message: 'you need to be authenticated to view this page',
-    icon: 'fa-solid fa-ban',
+    icon: <IconForbid size={iconSize} />,
     more: (
       <>
         you can login or register <Link to='/auth'>here</Link>
@@ -19,7 +22,7 @@ const errorMessages = {
   404: {
     heading: 'not found',
     message: "the page you're trying to access does not exist",
-    icon: 'fa-solid fa-circle-xmark',
+    icon: <IconError404 size={iconSize} />,
     more: 'use the navbar to browse',
   },
 };
@@ -28,10 +31,10 @@ export function AppError({ code }: AppErrorProps) {
   const error = errorMessages[code];
   return (
     <Paper shadow='xs' p='md'>
-      <Title>
-        <i className={error.icon} style={{ fontSize: '1.5rem' }} />{' '}
-        {error.heading}
-      </Title>
+      <Group align='flex-end' spacing='sm'>
+        {error.icon}
+        <Title>{error.heading}</Title>
+      </Group>
       <Text color='dimmed'>{error.message}</Text>
       <Text size='sm' mt='sm'>
         {error.more}
