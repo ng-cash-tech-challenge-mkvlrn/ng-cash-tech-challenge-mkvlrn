d654bdb0ae37396b57263f52a0556bd1b73e01f6
Mike Valeriano <mkvlrn@gmail.com>
2022-11-16 09:56:32 -0300

    feat: use root url as static client

--- src/backend/server/AppRouter.ts
+++ src/backend/server/AppRouter.ts
@@ -1,8 +1,7 @@
-import express, { Response, Router } from 'express';
-import { join, resolve } from 'path';
+import express, { Router } from 'express';
+import { join } from 'path';
 import { injectable } from 'tsyringe';
 
-import { CustomRequest } from '#/backend/interfaces/CustomRequest';
 import { AccountRouter } from '#/backend/modules/account/account.router';
 import { AuthRouter } from '#/backend/modules/auth/auth.router';
 import { UsersRouter } from '#/backend/modules/users/users.router';
@@ -21,8 +20,5 @@ export class AppRouter {
     this.routes.use('/api/account', this.accountRouter.routes);
 
     this.routes.use(express.static(join(__dirname, '..', 'client')));
-    this.routes.get('/client', (_req: CustomRequest, res: Response) => {
-      res.sendFile(resolve(__dirname, '..', 'client', 'index.html'));
-    });
   }
 }
