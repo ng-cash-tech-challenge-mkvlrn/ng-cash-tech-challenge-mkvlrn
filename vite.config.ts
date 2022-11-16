6b934e58dff30dd5c331af6ec7e14892788a3d09
Mike Valeriano <mkvlrn@gmail.com>
2022-11-16 09:57:06 -0300

    chore: use process.env inside vite build step

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
       '#': resolve(__dirname, './src'),
     },
   },
+  define: {
+    'process.env': process.env,
+  },
   base: '',
   build: {
     outDir: '../../build/client',
